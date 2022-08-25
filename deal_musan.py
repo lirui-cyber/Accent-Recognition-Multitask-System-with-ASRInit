8891c164237564a56c25657a8566ea314377844a
lirui-cyber <lruicor@163.com>
2022-08-25 21:09:13 +0800

    revise

--- deal_musan.py
+++ deal_musan.py
@@ -1,9 +1,15 @@
 import glob
 import os
-
-audio_dir = "/home3/jicheng/source-data/musan/noise/"
-data_dir = "/home3/jicheng/lirui/Accent-Recognition-Multitask-System-with-ASRInit/data/musan_noise/"
-audio_list = glob.glob(audio_dir+"**/*wav")
-with open(data_dir+"musan_noise_file_list.txt","w") as f:
-    for line in audio_list:
-        f.write(line+'\n')
+import sys
+import re
+audio_dir = sys.argv[1]
+data_dir = sys.argv[2]
+if not os.path.exists(data_dir):
+    os.mkdir(data_dir)
+audio_list = glob.glob(audio_dir+"/**/*wav")
+with open(data_dir+"/wav.scp","w") as f, open(data_dir+"/utt2spk","w") as F:
+    for audio in audio_list:
+        uttid = re.split('[/.]',audio)[-2]
+        f.write(uttid + " " + audio + "\n")
+        F.write(uttid + " " + uttid + "\n")
+            
